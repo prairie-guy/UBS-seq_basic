47ffdf131049b3781bee85db6524782270bf2759
prairie-guy <cdaniels@nandor.net>
2024-02-05 10:52:55 -0600

    Updating out_time() and _.called.csv

---
+++ scripts/run_ubs_seq.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd $(dirname "$(readlink -f "$0")")
+cd ../workspace/
+rm -fr fastqc_post/ fastqc_pre/ hisat3n_align/ hisat3n_call/ hisat3n_dedup/ hisat3n_sort/ trim/
+
+stamp=`date "+%d-%m-%Y-%H:%M"`
+
+ipython ubs_basic.py tee $date.log
