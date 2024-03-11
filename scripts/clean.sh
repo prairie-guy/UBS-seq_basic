b1bdd66eea499a9c447cd46edadc20a175c8c0ed
prairie-guy <cdaniels@nandor.net>
2024-03-11 14:11:21 -0500

    Backing up before doing combine_se_pd and combine_runs

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname "$(readlink -f "$0")")
 cd ../workspace/
-rm -fr call_converted  call_filtered_converted  dedup  fastqc_post  fastqc_pre  filter_calls  map_genes  map_genome samples trim
+rm -fr select_samples trim fastqc_post  fastqc_pre  map_se  map_se_pe map_combined call_converted  call_filtered_converted  dedup
--- scripts/configure.py
+++ scripts/configure.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-# Configure the samples data structures from config.yaml
-# Defines iterator generator: samples
+# Configure samples from config.yaml. Create data structure and corresponding access functions
+# Defines iterator generator: samples(), data() and group()
+# sample2data and group2sample data structures developed by Chang Ye
 
 import os, sys
 from pathlib import *
@@ -58,10 +59,11 @@ def samples(n=2, se=True, keys=False, extra=[]):
     - extra=[alist] returns an iterator which is the Cartesian product of the elements of alist and the original iterator
 
     Example:
-    samples(keys=True, extra=['gene','genome']) ->
+    samples(se=True, keys=True, extra=['gene','genome']) ->
      [('t1', 'r1', 'gene'), ('t1', 'r1', 'genome'), ('t2', 'r1', 'gene'), ('t2', 'r1', 'genome'), ('c1', 'r1', 'gene'), ('c1', 'r1', 'genome')]
     '''
-    if n not in [1, 2]: return "Invalid value for n"
+    if n not in [1, 2]: return("usage: samples(n=2, keys=False, extra=False)")
+    if not isinstance(se,bool) or not isinstance(keys,bool): return("usage: samples(n=2, keys=False, extra=False)")
     valid_extra = isinstance(extra, list) and len(extra) > 0
     # Generators
     # [n,keys,extra]
@@ -89,22 +91,41 @@ def samples(n=2, se=True, keys=False, extra=[]):
                     False:  [len(sample2data[s][r])==2 for s in sample2data for r in sample2data[s] for e in extra]}}
 
         mask = results[se][not extra]
-        return compress(gen,mask)
 
-def data(type='SE'):
+        if se: return compress(gen,mask)
+        if keys: return ((s,r,'R1','R2') for s,r in compress(gen,mask))
+        return ((f'{k}, ''R1','R2') for k in compress(gen,mask))
+
+def data(se=True, keys=False):
     """
     Generates iterator of data paths as defined in `sample2data`
-    - type='SE' will flatten the list and return a single path per tuple
-    - type='PE' will return a list of R1 and R2 and data paths
+    - type='SE' will flatten the list and return a key[s] with a single path to R1 sequence reads
+    - type='PE' will flatten the list and return a key[s] with a two paths to R1,R2  sequence reads
 
     Example:
-    data() -> [('t1_r1', PosixPath('/home/cdaniels/uofc_data/ubs_seq/UBS-seq_basic/scripts/../data/test1_R1.fq.gz')),
-               ('t2_r1', PosixPath('/home/cdaniels/uofc_data/ubs_seq/UBS-seq_basic/scripts/../data/test2_R1.fq.gz')),
-               ('c1_r1', PosixPath('/home/cdaniels/uofc_data/ubs_seq/UBS-seq_basic/scripts/../data/test3_R1.fq.gz'))]
+    data() -> [('t1_r1', PosixPath('../data/test1_R1.fq.gz')),
+               ('t2_r1', PosixPath('../data/test2_R1.fq.gz')),
+               ('c1_r1', PosixPath('../data/test3_R1.fq.gz'))]
+
+    data(se=False) ->
+              [('t1_r1', PosixPath('../data/test1.1_R1.fq.gz'), PosixPath('../data/test1.1_R2.fq.gz')),
+               ('t1_r2', PosixPath('../data/test_1.2_R1.fq.gz'), PosixPath('../data/test1.2_R2.fq.gz')),
+               ('t1_r3', PosixPath('../data/test1.3_R1.fq.gz'), PosixPath('../data/test1.3_R2.fq.gz'))]
     """
-    if type == 'SE': return ((f"{s}_{r}",d) for s, v in sample2data.items() for r, ds in v.items() for d in ds.values())
-    if type == 'PE': return ((f"{s}_{r}", ds) for s,v in sample2data.items() for r,ds in v.items())
-    return "usage: data(type), type = {'SE' | 'PE'}"
+    # if type == 'SE': return ((f"{s}_{r}",d) for s, v in sample2data.items() for r, ds in v.items() for d in ds.values())
+    # if type == 'PE': return ((f"{s}_{r}", ds) for s,v in sample2data.items() for r,ds in v.items())
+    # return "usage: data(type), type = {'SE' | 'PE'}"
+    if not isinstance(se,bool): return("usage: data(se=True), for PE  data(se=False)")
+    # [se][keys]
+    results = {
+        True: {False: ((f'{s}_{r}',sample2data[s][r]['R1']) for s,r in samples(se=True,keys=True)),
+               True:  ((s,r,sample2data[s][r]['R1']) for s,r in samples(se=True,keys=True))},
+        False: {False: ((f'{s}_{r}', sample2data[s][r][r1], sample2data[s][r][r2]) for s,r,r1,r2 in samples(se=False,keys=True)),
+                True:  ((s,r, sample2data[s][r][r1], sample2data[s][r][r2]) for s,r,r1,r2 in samples(se=False,keys=True))}}
+    return results[se][keys]
+    #if se:  return ((f'{s}_{r}',sample2data[s][r].values())  for s,r in samples(se=True,keys=True))
+    #if not se: return ((f'{s}_{r}',sample2data[s][r][r1], sample2data[s][r][r2]) for s,r,r1,r2 in samples(se=False,keys=True))
+
 
 def groups(n=2):
     if n == 1: return ((s, g) for g, items in group2sample.items() for s in items)
